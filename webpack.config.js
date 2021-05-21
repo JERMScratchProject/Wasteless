21f569ed7f3eca1f17d37d9fbb46182bab140511
Eileen Lee <eileenlee115@gmail.com>
2021-05-21 16:44:41 -0700

    web-dev server working

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: './client/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
+      {
+        test: /\.s?css/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  devServer: {
+    publicPath: '/',
+    proxy: {
+      '/api': 'http://localhost:3000',
+    },
+    hot: true,
+  },
+  plugins: [],
+  resolve: { extensions: ['.js', '.jsx'] },
+};
