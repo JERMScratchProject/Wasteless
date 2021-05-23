69b57e7d50cc70c1c734e6cb4f22d052df8d1966
Eileen Lee <eileenlee115@gmail.com>
2021-05-22 22:15:28 -0700

    adding more styling

--- components/Item.jsx
+++ components/Item.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 
-function Item (props) {
-
- 
-
+function Item(props) {
   return (
     <div className="ItemComponent">
-    <span>{ props.id }: { props.itemName } --- </span> <button> ✅ </button>
-    <button> ❌ </button>
+      <span className="items">
+        {props.id}. {props.itemName}{' '}
+      </span>{' '}
+      <div className="toBuyListBtnContainer">
+        <button className="toBuyListBtn">
+          {' '}
+          <i className="fa fa-check" />{' '}
+        </button>
+        <button className="toBuyListBtn">
+          {' '}
+          <i className="fa fa-edit" />{' '}
+        </button>
+        <button className="toBuyListBtn">
+          {' '}
+          <i className="fa fa-trash-o" />{' '}
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Item;
